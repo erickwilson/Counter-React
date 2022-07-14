a06273637cd80f7afe8e8db9c04fb65429f3918d
Erick Wilson <erick.97-11@hotmail.com>
2022-07-14 14:38:25 -0300

    counter react

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import SmartCounter from '../SmartCounter/SmartCounter';
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+
+
+function App() {
+  return (
+    <>
+    <h1>Contador!</h1>
+    <IfoodCounter/>
+    </> 
+  );
+}
+
+export default App;
